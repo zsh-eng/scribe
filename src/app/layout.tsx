9dac396432791b8360379b3ecc033ad0d59c2042
isaacyclai <isaac.yc.lai@gmail.com>
2026-02-02 11:27:22 +0800

    Change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Scribe",
   description: "Making Parliament Accessible for All",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🪶</text></svg>",
+  },
 };
 
 export default function RootLayout({
