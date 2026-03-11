29a38459738b7750b1b94c25eb29fd549ec57b3e
zsheng <zsheng.dev@gmail.com>
2026-03-11 10:25:12 +0800

    Add general prefetch strategy

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -7,6 +7,10 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "tap",
+  },
   build: {
     format: "file",
   },
